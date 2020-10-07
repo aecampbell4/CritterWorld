ef11db5f324477dad9faa432425d96c4315f8c6e
aecampbell4 <72481683+aecampbell4@users.noreply.github.com>
2020-10-06 22:59:35 -0400

    Delete package-info.java

--- package-info.java
+++
@@ -1,5 +0,0 @@
-package util.maybe;
-/*
-This package contains the Maybe implementation of the optional design pattern, created for CMS.
-Sourced from: https://www.cs.cornell.edu/courses/cs2112/2019fa/?resources
- */
